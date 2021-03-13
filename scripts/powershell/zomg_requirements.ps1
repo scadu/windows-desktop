c466d3ab9dcfe86bf1521712437af1c2315e25cb
Lukasz Jendrysik <scadu9@gmail.com>
2021-03-13 19:47:24 +0100

    Draft Requirements

---
+++ scripts/powershell/zomg_requirements.ps1
@@ -0,0 +1,30 @@
+# https://github.com/microsoft/Requirements
+
+if (-not(Get-Module -ListAvailable -Name Requirements)) {
+    Write-Output "Module Requirements not found. Installing..."
+    Install-Module -Name Requirements -Confirm:$False -Force -Scope CurrentUser
+}
+
+$ErrorActionPreference = "Stop"
+Import-Module Requirements
+
+
+
+$Requirements = @(
+    @{
+        Describe = "WSL is enabled"
+        Test     = { (Get-WindowsOptionalFeature -Online -FeatureName 'Microsoft-Windows-Subsystem-Linux' | Select-Object -ExpandProperty State) -eq "Enabled" }
+        Set      = {
+            Enable-WindowsOptionalFeature -Online -FeatureName 'Microsoft-Windows-Subsystem-Linux' -All | Out-Null
+        }
+    },
+    @{
+        Describe = "Virtual Machine Platform is enabled"
+        Test     = { (Get-WindowsOptionalFeature -Online -FeatureName 'VirtualMachinePlatform' | Select-Object -ExpandProperty State) -eq "Enabled" }
+        Set      = {
+            Enable-WindowsOptionalFeature -Online -FeatureName 'VirtualMachinePlatform' -All | Out-Null
+        }
+    }
+)
+
+$Requirements | Invoke-Requirement | Format-Checklist
