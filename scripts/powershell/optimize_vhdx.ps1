bcc301ea9ea212b682e6a51168b253d96a1d9a4e
Lukasz Jendrysik <scadu9@gmail.com>
2022-01-16 11:02:50 +0100

    Pave road for optimize vhdx

--- scripts/powershell/optimize_vhdx.ps1
+++ scripts/powershell/optimize_vhdx.ps1
@@ -1,4 +1,8 @@
 # https://www.blogabout.cloud/2020/05/1460/
 wsl --shutdown
-Get-Process *docker* | Stop-Process
-Get-ChildItem -Path $HOME\AppData\Local -Include ext4.vhdx -File -Recurse -Depth 3 -ErrorAction SilentlyContinue | ForEach-Object { Optimize-VHD -Path $_.fullname -Mode full }
+# 'com.docker.service' is running as SYSTEM and it's not required to stop that process to proceed.
+$DockerProcesses = Get-Process -Name *docker* | Where-Object ProcessName -NE 'com.docker.service'
+# Get-Process -Name *docker* | Where-Object ProcessName -NE 'com.docker.service' | Stop-Process
+Stop-Process -InputObject $DockerProcesses
+# Get-Process | Where-Object { $_.HasExited }
+# Get-ChildItem -Path $env:LOCALAPPDATA -Include ext4.vhdx -File -Recurse -Depth 3 -ErrorAction SilentlyContinue | ForEach-Object -Parallel { Optimize-VHD -Path $_.fullname -Mode full }
