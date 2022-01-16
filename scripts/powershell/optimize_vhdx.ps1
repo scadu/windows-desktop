309d833c48467b7ca7d5eb990b1962d281a2a723
Lukasz Jendrysik <scadu9@gmail.com>
2022-01-16 15:11:50 +0100

    Add warning to the script

--- scripts/powershell/optimize_vhdx.ps1
+++ scripts/powershell/optimize_vhdx.ps1
@@ -1,3 +1,6 @@
+$WarningMessage = "In order to optimize VHD images, it's required to stop WSL instances and Docker Desktop."
+Write-Warning $WarningMessage -WarningAction Inquire
+
 # 'com.docker.service' is running as SYSTEM and it's not required to stop that process to continue.
 $DockerProcesses = Get-Process -Name *docker* | Where-Object ProcessName -NE 'com.docker.service'
 
