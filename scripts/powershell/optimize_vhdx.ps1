0251151ba5b51088e1e33d891659c613214989f6
Łukasz Jendrysik <scadu9@gmail.com>
2022-02-02 09:22:00 +0000

    Add todo, reorganize VHD images listing

--- scripts/powershell/optimize_vhdx.ps1
+++ scripts/powershell/optimize_vhdx.ps1
@@ -1,4 +1,13 @@
-$WarningMessage = "In order to optimize VHD images, it's required to stop WSL instances and Docker Desktop."
+# TODO: Add comments
+# TODO: Wrap up steps in functions
+# TODO: Handle case when there's no VHD images found
+
+$VHDImages = Get-ChildItem -LiteralPath $env:LOCALAPPDATA -Filter ext4.vhdx -File -Recurse -Depth 3 -ErrorAction SilentlyContinue
+# Fancy formatting, indeed. "{0:N2}" means two decimal places of zeros to show.
+# https://hochwald.net/powershell-get-the-size-of-given-folder-in-human-readable-format/
+Write-Output = "Found following VHDX images:"  $(VHDImages | Select-Object FullName, @{Name="GigaBytes";Expression={ "{0:N2}" -f ($_.Length / 1GB)}})
+
+$WarningMessage = "Would you like to proceed? In order to optimize VHD images, it's required to stop WSL instances and Docker Desktop."
 Write-Warning $WarningMessage -WarningAction Inquire
 
 # 'com.docker.service' is running as SYSTEM and it's not required to stop that process to continue.
@@ -10,5 +19,5 @@ if ($DockerProcesses ) {
 }
 
 wsl --shutdown
-Get-ChildItem -Path $env:LOCALAPPDATA -Include ext4.vhdx -File -Recurse -Depth 3 -ErrorAction SilentlyContinue |
-ForEach-Object -AsJob -Parallel { Optimize-VHD -Path $_.fullname -Mode full; Write-Output $_.fullname } | Receive-Job -Wait -AutoRemoveJob
+$VHDImages | ForEach-Object -AsJob -Parallel { Optimize-VHD -Path $_.fullname -Mode full; Write-Output $_.fullname } |
+Receive-Job -Wait -AutoRemoveJob
